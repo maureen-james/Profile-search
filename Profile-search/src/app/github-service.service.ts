ff7ea590d4a34e28f7013556ebcbb24d4c340df6
maureen-james <manjax97@gmail.com>
2022-04-18 14:12:39 +0300

    write a logic to fetch profiles

--- Profile-search/src/app/github-service.service.ts
+++ Profile-search/src/app/github-service.service.ts
@@ -1,9 +1,36 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { count } from 'console';
+import { catchError, retry, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubServiceService {
 
-  constructor() { }
+  constructor(private httpClient:HttpClient) { }
+// profile
+public getProfile(searchQuery: any){
+  let dataURL = `https://api.github.com/user/${searchQuery}?client_id=${CLIENT_ID}&client_secret${CLIENT_SECRET}`;
+  return this.httpClient.get(dataURL).pipe(
+    retry( count:1),
+    catchError(this.handleErrors)
+  );
+}
+// repos
+public handleErrors(error:HttpErrorResponse){
+  let errorMessage:string;
+  if (error.error instanceof ErrorEvent){
+    errorMessage = `MESSAGE : ${error.error.message}`;
+
+
+  }
+  else{
+    errorMessage = `STATUS: ${error.status} MESSAGE: ${error.message}`
+  }
+  return throwError(errorMessage);
+  )
+}
+
+
 }
